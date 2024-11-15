c7f7048b871aeaa1ef31eea9ddea1ff060546fea
Vítor Ian <82915119+vitor-ian@users.noreply.github.com>
2024-11-15 17:45:05 -0300

    add files

---
+++ assets/i18n.f6870bb6.js
@@ -0,0 +1,1 @@
+import{P as l}from"./index.d0d33634.js";import{c as r}from"./vue-i18n.runtime.eb49b006.js";var o={},s={},t={"pt-BR":s,"en-US":o},f=({app:a})=>{const e=r({locale:l.getLocale(),fallbackLocale:"en-US",legacy:!1,messages:t});a.use(e)};export{f as default};
