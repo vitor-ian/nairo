dc2a4e43549df73be11e9934c2c97f0368a26209
Vítor Ian <82915119+vitor-ian@users.noreply.github.com>
2024-11-15 17:54:55 -0300

    fix: img path

---
+++ assets/i18n.705c9f0b.js
@@ -0,0 +1,1 @@
+import{P as l}from"./index.b3f3d284.js";import{c as r}from"./vue-i18n.runtime.78f294b7.js";var o={},s={},t={"pt-BR":s,"en-US":o},f=({app:a})=>{const e=r({locale:l.getLocale(),fallbackLocale:"en-US",legacy:!1,messages:t});a.use(e)};export{f as default};
